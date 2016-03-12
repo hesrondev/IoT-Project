e3d3d3796bdc5c3a4e021663284b91d26d550c27
hesrondev <hesron@live.fr>
2016-03-12 23:30:55 +0100

    ajout des spécification des performances réseau

--- client/graphics/networktabdetails.cpp
+++ client/graphics/networktabdetails.cpp
@@ -4,8 +4,37 @@ NetworkTabDetails::NetworkTabDetails(QString title, QString subTitle, QString gr
     PerformanceGraph(title, subTitle, graphLegend)
 {
 
+    //
+    QLabel *dataInLabel = new QLabel("Recevoir");
+    QLabel *dataInValueLabel = new QLabel("566 bits/s");
+    QLabel *dataOutLabel = new QLabel("Envoyer");
+    QLabel *dataOutValueLabel = new QLabel("126 bits/s");
+
+    // Spec
+
+    QLabel *ipv4Label = new QLabel("Adresse IPv4:");
+    QLabel *ipv6Label = new QLabel("Adresse IPv6:");
+    QLabel *ipv4ValueLabel = new QLabel("196.3.65.2");
+    QLabel *ipv6ValueLabel = new QLabel("2001:0db8:0000:85a3:0000:0000:ac1f:8001:");
+
+    // layering specifications
+
+    QGridLayout *glayout = new QGridLayout();
+    glayout->setMargin(0);
+    glayout->setHorizontalSpacing(20);
+    glayout->setAlignment(Qt::AlignLeft);
+
+    glayout->addWidget(dataInLabel, 0, 0);
+    glayout->addWidget(dataOutLabel, 0, 1);
+    glayout->addWidget(ipv4Label, 0, 2);
+    glayout->addWidget(ipv4ValueLabel, 0, 3);
+
+    glayout->addWidget(dataInValueLabel, 1, 0, 2, 1);
+    glayout->addWidget(dataOutValueLabel, 1, 1, 2, 1);
+    glayout->addWidget(ipv6Label, 1, 2);
+    glayout->addWidget(ipv6ValueLabel, 1, 3);
 
     // Layering
-
+    vlayout->addLayout(glayout);
     vlayout->addStretch(1);
 }
